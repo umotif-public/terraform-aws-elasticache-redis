8bca23293d0ac9e8d74955bbeda384d82e535c13
Volodymyr Stetsenko <uastetsenko@gmail.com>
2021-07-26 16:33:48 +0300

    Updated name for SG

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_elasticache_parameter_group" "redis" {
 }
 
 resource "aws_elasticache_subnet_group" "redis" {
-  name        = "${var.name_prefix}-redis-sg"
+  name        = var.global_replication_group_id == null ? "${var.name_prefix}-redis-sg" : "${var.name_prefix}-redis-sg-replica"
   subnet_ids  = var.subnet_ids
   description = var.description
 
