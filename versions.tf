ae267e535d186ff05763494b5f53e7885079b3ee
Abdul Wahid <abdul.wahid@umotif.com>
2022-03-09 13:00:42 +0000

    Upgrade module to be compatible with AWS Provider 4.0.0

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 0.14.5"
+  required_version = ">= 0.14.11"
 
   required_providers {
-    aws    = ">= 3.43"
+    aws    = ">= 4.0.0"
     random = ">= 3.1.0"
   }
 }
